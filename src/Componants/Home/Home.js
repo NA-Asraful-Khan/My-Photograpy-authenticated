cc59793280a2cf8a8540e7ca9427d695e94fa746
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-04-05 16:12:40 +0600

    home bottom Review section done

--- src/Componants/Home/Home.js
+++ src/Componants/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HomeBottom from '../HomeBottom/HomeBottom';
 import HomeTop from '../HomeTop/HomeTop';
 import './Home.css'
@@ -7,7 +8,11 @@ const Home = () => {
     return (
         <div>
             <HomeTop></HomeTop>
-            <HomeBottom></HomeBottom>
+            <HomeBottom>
+            </HomeBottom>
+            <div className='reviewDiv'>  
+            <Link className='reviewButton' to="/Reviews">Reviews</Link>
+            </div>
         </div>
     );
 };
--- src/Componants/HomeBottom/HomeBottom.js
+++ src/Componants/HomeBottom/HomeBottom.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useReviewData from '../../customhook/ReviewData';
+import Review from '../Review/Review';
 import './HomeBottom.css'
 
 const HomeBottom = () => {
+    const [reviews, setReview] = useReviewData();
+    const slicing = reviews.slice(0, 3);
     return (
         <div className='home-bottom'>
-            <h2>Reviews</h2>
+            <h2>Reviews({slicing.length})</h2>
+            <div className='review-container'>
+                {
+                    slicing.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };
