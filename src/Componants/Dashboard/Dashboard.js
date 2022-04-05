ca61116888c39efbb109d5ea3883cd11773f666a
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-04-05 17:45:58 +0600

    404 edited

--- src/Componants/Dashboard/Dashboard.js
+++ src/Componants/Dashboard/Dashboard.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import useRechartData from '../../customhook/ChartData';
 import './Dashboard.css'
-
 import { Line,Area, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart } from 'recharts';
-
-const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
 
 const Dashboard = () => {
     // [month, investment, sell, revenue]
--- src/Componants/NotFound/NotFound.js
+++ src/Componants/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Error...</h2>
-            <h2>The link you have been click is not found</h2>
+        <div className='notFound pd'>
+            <img src="https://cdn-icons-png.flaticon.com/512/755/755064.png" alt="" />
+            <h2>This page is outside of the Universe</h2>
+            <h3>The page you are trying to access does not exist or has been moved.</h3>
+            <h3>Try going back to our homepage.</h3>
+            <Link to="/Home">Home</Link>
+
         </div>
     );
 };
