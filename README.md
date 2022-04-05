5206b8734a89c6892df0ca343b792d5d2e25866b
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-04-05 19:01:56 +0600

    About section done

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # My Photograpy
 
-My Live site link is:  [Create React App](https://github.com/facebook/create-react-app).
+My Live [site link](https://github.com/facebook/create-react-app) is:  https://photoprahy-routing.netlify.app/.
 
 ## About This Project
 
 * I use React Routing. That was fun to use.
-* I did destructuring.
+* I create 404 route for unavailable route.
 * I use Custome Hooks for reuse the same type of work again.
 * I use Rechart for dashboard
--- src/Componants/About/About.js
+++ src/Componants/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div className='pd'>
-            <h2>This is About</h2>
+        <div className='pd about'>
+            <img src="https://cdn-icons.flaticon.com/png/512/4140/premium/4140048.png?token=exp=1649164235~hmac=d0d2a967dc84dabf18c7966257fcafee" alt="" />
+
+            <h2>Nur A Asraful Khan</h2>
+            <h2>SHIKHOR SOYON (Nick Name)</h2>
+            <h3>Study: BSc in CSE (1st sem)</h3>
+            <h3>JOB: Graphics Designer</h3>
+            <h3>GOAL: To Be A Web Developer</h3>
+            <h3>Training with Programming Hero</h3>
+            <h3>Mentor: Honourable Jhankar Mahbub Sir</h3>
         </div>
     );
 };
